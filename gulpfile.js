c3371d5b3aeba00eb82760de496508fe477670e2
Daniel Weaver <danweaver06@gmail.com>
2014-07-10 20:21:02 -0400

    server

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
-var gulp = require('gulp');
-var karma = require('gulp-karma');
+var gulp = require('gulp'),
+    karma = require('gulp-karma'),
+    connect = require('gulp-connect');
+
+gulp.task('server', function(){
+  connect.server({
+    root: ['misc/demo']
+  });
+});
 
 gulp.task('test', function() {
   // Be sure to return the stream
   return gulp.src('./foobar')
+
     .pipe(karma({
       configFile: 'karma.conf.js',
       action: 'run'
     }))
+    
     .on('error', function(err) {
       // Make sure failed tests cause gulp to exit non-zero
       throw err;
     });
 });
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task('default', ['test']);
